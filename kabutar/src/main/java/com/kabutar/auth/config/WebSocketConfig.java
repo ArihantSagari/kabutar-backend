fac2495d0c8f2c45b5a270a43cd95175ccf99d62
ArihantSagari <155970293+ArihantSagari@users.noreply.github.com>
2026-01-20 04:34:52 +0530

    update
    update

--- kabutar/src/main/java/com/kabutar/auth/config/WebSocketConfig.java
+++ kabutar/src/main/java/com/kabutar/auth/config/WebSocketConfig.java
@@ -10,26 +10,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-	    registry
-	            .addEndpoint("/ws")
-	            .setAllowedOriginPatterns("http://localhost:3000")
-	            .addInterceptors(new com.kabutar.auth.ws.WsHandshakeInterceptor())
-	            .setHandshakeHandler(new com.kabutar.auth.ws.WsUserHandshakeHandler())
-	            .withSockJS();
-	}
-
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://kabutar-random-chat.vercel.app"
+                )
+                .withSockJS();
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // client -> server prefix
         registry.setApplicationDestinationPrefixes("/app");
-
-        // server -> client prefix
         registry.enableSimpleBroker("/topic", "/queue");
-
-        // for /user/queue/...
         registry.setUserDestinationPrefix("/user");
     }
 }
